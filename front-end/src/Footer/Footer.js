4341154ba5e9983f5e6d177764abf0b7f460a077
Sathwik Reddy Danda <sathwikreddydanda@Sathwiks-Air.lan>
2023-12-12 23:44:33 -0500

    Final commit

---
+++ front-end/src/Footer/Footer.js
@@ -0,0 +1,14 @@
+function Footer() {
+    return (
+        <footer className="bottom">
+    
+        <div className="end">
+            All rights reserved &copy; Sathwik Reddy Danda
+        </div>
+    
+</footer>
+  
+    );
+  }
+  
+  export default Footer;
